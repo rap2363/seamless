e08e7b8a5b7be93c5f85eda2d07d6041756264c3
Rohan Paranjpe <rap2363@gmail.com>
2020-04-12 21:45:30 -0700

    Seam Removal

--- src/main/java/application/SeamCarver.java
+++ src/main/java/application/SeamCarver.java
@@ -7,6 +7,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public final class SeamCarver {
     public static void main(final String[] args) throws IOException {
@@ -18,9 +19,25 @@ public final class SeamCarver {
         final String filePath = args[0];
         final BufferedImage image = ImageIO.read(new File(filePath));
         final String newFile = "new_altered.png";
-        final BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
-        final int[] rgbPixels = Images.getRGBPixelsFromBuferedImage(image);
-        final ConnectedImage connectedImage = new ConnectedImage(image.getHeight(), image.getWidth(), rgbPixels);
+        ConnectedImage connectedImage = new ConnectedImage(image.getHeight(), image.getWidth(), image);
+        final Random random = new Random(0);
+        final int numVerticalSeams = 300;
+        for (int i = 0; i < numVerticalSeams; i++) {
+            final int[] verticalSeam = new int[connectedImage.getHeight()];
+            int walkIndex = 250;
+            for (int y = 0; y < connectedImage.getHeight(); y++) {
+                verticalSeam[y] = walkIndex;
+                walkIndex += random.nextInt(3) - 1;
+            }
+
+            connectedImage = connectedImage.removeVerticalSeam(verticalSeam);
+        }
+
+        final BufferedImage newImage = new BufferedImage(
+                connectedImage.getWidth(),
+                connectedImage.getHeight(),
+                image.getType()
+        );
 
         for (int y = 0; y < newImage.getHeight(); y++) {
             for (int x = 0; x < newImage.getWidth(); x++) {
--- src/main/java/core/structures/ConnectedImage.java
+++ src/main/java/core/structures/ConnectedImage.java
@@ -2,28 +2,42 @@ package core.structures;
 
 import core.Images;
 
+import java.awt.image.BufferedImage;
+
 /**
  * A ConnectedImage is a patchwork of linked PixelNodes (each pixel has 8 pointers to its neighbors). This is useful for
  * being able to create seam paths through an image or remove seams from an image.
  * <p>
- * Note: A ConnectedImage is an immutable object. We can remove or add one or multiple seams at once, but this operation
- * creates a deep copy of the underlying pixel nodes.
+ * Note: A ConnectedImage is *not* an immutable object. We can remove or add seams, but thesee operation mutate the
+ * underlying pixel nodes.
  */
 public final class ConnectedImage {
     public final PixelNode[] pixelNodes;
     private final int height;
     private final int width;
 
-    public ConnectedImage(final int height, final int width, final int[] imageRGBPixels) {
+    private ConnectedImage(final int height,
+                           final int width,
+                           final PixelNode[] pixelNodes) {
         this.height = height;
         this.width = width;
+        this.pixelNodes = pixelNodes;
+    }
+
+    public ConnectedImage(final int height, final int width, final BufferedImage image) {
+        this(height, width, createPixelNodesFromBufferedImage(height, width, image));
+    }
+
+    private static PixelNode[] createPixelNodesFromBufferedImage(final int height,
+                                                                 final int width,
+                                                                 final BufferedImage image) {
         final PixelNode[] pixelNodes = new PixelNode[height * width];
 
         // First set the pixel nodes array
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 final int imageIndex = Images.getIndex(x, y, width);
-                pixelNodes[imageIndex] = new PixelNode(imageRGBPixels[imageIndex]);
+                pixelNodes[imageIndex] = new PixelNode(image.getRGB(x, y));
             }
         }
 
@@ -66,18 +80,91 @@ public final class ConnectedImage {
             }
         }
 
-        this.pixelNodes = pixelNodes;
+        return pixelNodes;
     }
 
     private static PixelNode getPixelNode(final PixelNode[] pixelNodes, final int x, final int y, final int width) {
         return pixelNodes[Images.getIndex(x, y, width)];
     }
 
-    /**
-     * Create an entirely new ConnectedImage by removing a vertical seam.
-     */
-    public static ConnectedImage removeVerticalSeam(final VerticalSeam verticalSeam) {
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    private PixelNode getPixelNodeAt(final int x, final int y) {
+        return pixelNodes[Images.getIndex(x, y, width)];
+    }
+
+    private PixelNode getPixelNodeOrNullAt(final int x, final int y) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
+            return getPixelNodeAt(x, y);
+        }
+
         return null;
+    }
+
+    public int getRgbPixelAt(final int x, final int y) {
+        return getPixelNodeAt(x, y).getRGBValue();
+    }
+
+    /**
+     * Create an entirely new ConnectedImage by removing a vertical seam. This mutates the underlying ConnectedImage.
+     */
+    public ConnectedImage removeVerticalSeam(final int[] verticalSeam) {
+        final int height = this.getHeight();
+        final int newWidth = this.getWidth() - 1;
+        if (newWidth <= 0) {
+            throw new IllegalArgumentException("New image after seam removal will have 0 width!");
+        }
+
+        final PixelNode[] newImagePixelNodes = new PixelNode[height * newWidth];
+
+        int i = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < this.getWidth(); x++) {
+                if (verticalSeam[y] == x) {
+                    final int previousVerticalSeamX = y > 0 ? verticalSeam[y - 1] : Integer.MAX_VALUE;
+                    restitchRightShiftingPixelNode(x, y, previousVerticalSeamX);
+                    continue;
+                }
+
+                newImagePixelNodes[i++] = getPixelNodeAt(x, y);
+            }
+        }
+
+        return new ConnectedImage(height, newWidth, newImagePixelNodes);
+    }
+
+    /**
+     * This method restiches the underlying pixel node framework to remove a pixel node located at x, y. It does this
+     * by restiching the pixels on the right and left of the pixel to the nodes above it properly. This assumes that
+     * we are removing pixels in a vertical seam from top to bottom (which we are by definition of a vertical seam).
+     */
+    private void restitchRightShiftingPixelNode(final int x, final int y, final int previousX) {
+        final PixelNode left = getPixelNodeOrNullAt(x - 1, y);
+        final PixelNode right = getPixelNodeOrNullAt(x + 1, y);
+
+        if (left != null) {
+            final int offsetX = previousX >= x ? 1 : 0;
+
+            left.setRight(right);
+            left.setUpper(getPixelNodeOrNullAt(x + offsetX, y - 1));
+            left.setUpperRight(getPixelNodeOrNullAt(x + offsetX + 1, y - 1));
+            // We don't need to reset the upper left, because this pixel is on the left of the pixel to remove.
+        }
+
+        if (right != null) {
+            final int offsetX = previousX >= x ? -1 : 0;
+
+            right.setLeft(left);
+            right.setUpper(getPixelNodeOrNullAt(x + offsetX, y - 1));
+            right.setUpperLeft(getPixelNodeOrNullAt(x + offsetX + 1, y - 1));
+            // We don't need to reset the upper right, because this pixel is on the right of the pixel to remove.
+        }
     }
 }
 
